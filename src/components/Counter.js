344fe2c32e2648279ce3ada96b523b7827d16f4d
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-11 10:43:55 -0500

    commit tree edc4f42828a3e2ab30de9571cec2fb6e46a13d1a

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,9 +3,21 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  // function increment() {
+  //   setCount((currentCount) => currentCount + 1);
+  //   setCount((currentCount) => currentCount + 1);
+  // }
   function increment() {
-    setCount(count + 1);
+    // every time we're calculating previously value, it's best practice to call a callback function to calculate the previous value first
+    // instead of this:
+    // setCount(count + 1)
+    setCount(count => count + 1);
   }
+
+  // when the <button> element is clicked, it will run our increment function
+  // increment calls the setCount function to:
+  // 1. Update the value of count in React's internal state to be count + 1
+  // 2. Re-render our component
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
